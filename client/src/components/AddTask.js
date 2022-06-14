386d42e1e23ae58ec64b8c4d8c1cebaeff5a25ae
Ibon2 <ibonlkz@gmail.com>
2022-06-14 17:20:08 +0200

    structure of Add Task

--- client/src/components/AddTask.js
+++ client/src/components/AddTask.js
@@ -1,9 +1,40 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 function AddTask() {
+    const[title,setTitle] = useState("");
+    const[description,setDescription] = useState("");
+    const navigate = useNavigate();
+    function titleForm(){
+        return(
+            <input
+             value={title}
+             onChange={e => setTitle(e.target.value)}
+             placeholder="Title"
+             type="text"
+             name="title"
+             required
+           />
+        )
+    }
+    function descriptionForm(){
+        return(
+            <input
+             value={description}
+             onChange={e => setDescription(e.target.value)}
+             placeholder="description"
+             type="text"
+             name="description"
+             required
+           />
+        )
+    }
     return (
-        <div>
-            AddTask
-        </div>
+        <form onSubmit={()=>{navigate('/')}}>
+        {titleForm()}
+        {descriptionForm()}
+        <button> Submit </button>
+        </form>
     );
 }
 
