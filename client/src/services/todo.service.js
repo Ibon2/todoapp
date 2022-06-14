9bc612ff81c1ec907e95c43552bf586859919c2e
kevinleey <55768372+kevinleey@users.noreply.github.com>
2022-06-14 18:57:41 +0200

    Added todo.service.js to manage Axios calls

---
+++ client/src/services/todo.service.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const baseURL = 'http://localhost:5000/todos/';
+
+export const todoService = {
+    getAll() {
+        return axios.get(baseURL);
+    },
+    getOne(todoTitle) {
+        return axios.get(baseURL + `/${todoTitle}`);
+    },
+    add(todoDoc) {
+        return axios.post(baseURL, todoDoc);
+    },
+    delete(todoTitle) {
+        return axios.delete(baseUrl + `/${todoTitle}`);
+    },
+    update(todoDoc) {
+        return axios.put(baseUrl, todoDoc);
+    }
+}
